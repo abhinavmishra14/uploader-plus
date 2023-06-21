7b55eb0a5845586fdc67247b769c1be4ac747917
abhinav kumar mishra <abhinavmishra14@gmail.com>
2023-06-21 17:42:00 -0400

    Debug upload fix.

--- uploader-plus-platform/src/main/resources/alfresco/extension/templates/webscripts/com/softwareloop/uploader-plus/upload.post.js
+++ uploader-plus-platform/src/main/resources/alfresco/extension/templates/webscripts/com/softwareloop/uploader-plus/upload.post.js
@@ -3,12 +3,15 @@
 // When end user overrides via extension directory, this won't pick up the override.
 // This is a drawback of Alfresco JS importing.
 
+
 function uploaderPlusMain()
 {
     var repoFormData, fnFieldValue, idx, max, field, fieldName, value;
     
     repoFormData = new Packages.org.alfresco.repo.forms.FormData();
-    
+    if(logger.isLoggingEnabled()) {
+		logger.log("[Uploader plus] - uploaderPlusMain started...");
+	}
     try {
         
         // Prevents Flash- and IE8-sourced "null" values being set for those parameters where they are invalid.
@@ -49,16 +52,24 @@ function uploaderPlusMain()
                 default:
                     // any other field may be a form field (assoc_ or prop_ or any transient field)
                     value = fnFieldValue(field);
+					if(logger.isLoggingEnabled()) {
+						logger.log("[Uploader plus] - field: "+field+" | value: "+value);
+					}
                     if (value !== null)
                     {
                         repoFormData.addFieldData(fieldName, value);
                     }
             }
         }
-
+		if(logger.isLoggingEnabled()) {
+			logger.log("[Uploader plus] - Saving form, repoFormData: "+repoFormData);
+		}
         formService.saveForm("node", model.document.nodeRef, repoFormData);
     }
     catch (e) {
+		if(logger.isLoggingEnabled()) {
+			logger.log("[Uploader plus] - Error occurred while uploading due to : "+e.message);
+		}
         // capture exception, annotate it accordingly and re-throw
         if (e.message && e.message.indexOf("AccessDeniedException") != -1) {
             e.code = 403;
@@ -67,9 +78,29 @@ function uploaderPlusMain()
             // very unlikely
             e.code = 404;
         }
+	    else if (e.message && e.message.indexOf("FileExistsException") != -1) {
+            //Rename and upload.
+			var fieldDataName = repoFormData.getFieldData("name");
+			var dotIndex = fieldDataName.lastIndexOf(".");
+			var tmpFilename = "";
+			  if (dotIndex > 0)
+		      {
+		         // Filename contained ".", create "filename-1.txt"
+		         tmpFilename = fieldDataName.substring(0, dotIndex) + "-" + "1" + fieldDataName.substring(dotIndex);
+		      }
+		      else
+		      {
+		         // Filename didn't contain a dot at all, create "filename-1"
+		         tmpFilename = fieldDataName + "-" + "1";
+		      }
+			//Add updated name
+			repoFormData.addFieldData("name", tmpFilename, true);
+	        formService.saveForm("node", model.document.nodeRef, repoFormData);
+
+        }
         else {
             e.code = 500;
-            e.message = "Unexpected error occurred during upload of new content.";
+            e.message = "Unexpected error occurred during upload of new content in uploader plus.";
         }
         throw e;
     }
